fffe0897fc75b05838d999cfbf54a434ae42c0e1
Jane mathenge <waguthijane61@gmail.com>
2025-08-11 21:30:57 +0300

    my work

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Jane Mathenge</h1>
+      <h2>Software Engineering</h2>
+
+    
+    </>
+  )
+}
+
+export default App
